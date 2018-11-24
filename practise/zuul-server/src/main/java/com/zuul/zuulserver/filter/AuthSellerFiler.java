b6a9a1a52f0a3ec24858f3d169a1b5a97dc729b2
LEN0V0 <zhangshicong888@sina.com>
2018-11-24 22:12:45 +0800

    test

--- practise/zuul-server/src/main/java/com/zuul/zuulserver/filter/AuthSellerFiler.java
+++ practise/zuul-server/src/main/java/com/zuul/zuulserver/filter/AuthSellerFiler.java
@@ -2,9 +2,10 @@ package com.zuul.zuulserver.filter;
 
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import com.sun.xml.internal.fastinfoset.stax.events.Util;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -12,11 +13,13 @@ import static org.springframework.cloud.netflix.zuul.filters.support.FilterConst
 import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
 
 /**
- * Created by
- *
+ * @描述
+ * @创建人 shicong.zhang
+ * @创建时间 $date$
+ * @修改人和其它信息
  */
 @Component
-public class TokenFilter extends ZuulFilter {
+public class AuthSellerFiler extends ZuulFilter {
     @Override
     public String filterType() {
         return PRE_TYPE;
@@ -29,20 +32,24 @@ public class TokenFilter extends ZuulFilter {
 
     @Override
     public boolean shouldFilter() {
-        return true;
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+        String uri = request.getRequestURI();
+        if("/myorder/test/testListProducts".equals(uri)){
+            return true;
+        }
+        return false;
     }
 
     @Override
-    public Object run() {
+    public Object run() throws ZuulException {
         RequestContext requestContext = RequestContext.getCurrentContext();
         HttpServletRequest request = requestContext.getRequest();
-
-        //这里从url参数里获取, 也可以从cookie, header里获取
-//        String token = request.getParameter("token");
-//        if (StringUtils.isEmpty(token)) {
-//            requestContext.setSendZuulResponse(false);
-//            requestContext.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value());
-//        }
+        String token = request.getParameter("token");
+        if(Util.isEmptyString(token)){
+            requestContext.setSendZuulResponse(false);
+            requestContext.setResponseStatusCode(HttpStatus.BAD_GATEWAY.value());
+        }
         return null;
     }
 }
