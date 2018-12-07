c9cb6cb1a8848624679f637be4ffbe9a5070e15a
jason <shicong.zhang@juniorchina.com>
2018-12-07 09:50:11 +0800

    test

--- practise/order/src/main/java/com/eureka/order/controller/HystrixController.java
+++ practise/order/src/main/java/com/eureka/order/controller/HystrixController.java
@@ -2,6 +2,7 @@ package com.eureka.order.controller;
 
 //import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 //import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,20 +29,20 @@ public class HystrixController {
 //			@HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000"), //休眠时间窗
 //			@HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60"),	//错误率
 //	})
-//	@HystrixCommand
-//	@GetMapping("/getProductInfoList")
-//	public String getProductInfoList(@RequestParam("number") Integer number) {
-//		if (number % 2 == 0) {
-//			return "success";
-//		}
-//
-//		RestTemplate restTemplate = new RestTemplate();
-//		return restTemplate.postForObject("http://127.0.0.1:8005/product/listForOrder",
-//				Arrays.asList("157875196366160022"),
-//				String.class);
-//
-////		throw new RuntimeException("发送异常了");
-//	}
+	@HystrixCommand
+	@GetMapping("/getProductInfoList")
+	public String getProductInfoList(@RequestParam("number") Integer number) {
+		if (number % 2 == 0) {
+			return "success";
+		}
+
+		RestTemplate restTemplate = new RestTemplate();
+		return restTemplate.postForObject("http://127.0.0.1:8005/product/listForOrder",
+				Arrays.asList("157875196366160022"),
+				String.class);
+
+//		throw new RuntimeException("发送异常了");
+	}
 
 	private String fallback() {
 		return "太拥挤了, 请稍后再试~~";
