4c6e77c9bb80a8cf2a19add07a62443a3b2da95f
jason <shicong.zhang@juniorchina.com>
2019-01-23 13:30:06 +0800

    test

--- practise/order/src/main/java/com/eureka/order/controller/HystrixController.java
+++ practise/order/src/main/java/com/eureka/order/controller/HystrixController.java
@@ -1,8 +1,9 @@
 package com.eureka.order.controller;
 
-//import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
-//import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +16,12 @@ import java.util.Arrays;
  *
  */
 @RestController
-//@DefaultProperties(defaultFallback = "defaultFallback")
+@DefaultProperties(defaultFallback = "defaultFallback")
 public class HystrixController {
 
-	//超时配置
+	//超时配置  默认为1000ms = 1秒   如果配置文件配置了超时  →  时间那么此处就失效了
 //	@HystrixCommand(commandProperties = {
-//			@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+//			@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1000")
 //	})
 
 //	@HystrixCommand(commandProperties = {
@@ -31,15 +32,17 @@ public class HystrixController {
 //	})
 	@HystrixCommand
 	@GetMapping("/getProductInfoList")
-	public String getProductInfoList(@RequestParam("number") Integer number) {
+	public String getProductInfoList(@RequestParam("number") Integer number) throws InterruptedException {
 		if (number % 2 == 0) {
 			return "success";
 		}
+        Thread.sleep(2000);
+		return  "ok";
+//		RestTemplate restTemplate = new RestTemplate();
+//		return restTemplate.postForObject("http://127.0.0.1:8005/product/listForOrder",
+//				Arrays.asList("157875196366160022"),
+//				String.class);
 
-		RestTemplate restTemplate = new RestTemplate();
-		return restTemplate.postForObject("http://127.0.0.1:8005/product/listForOrder",
-				Arrays.asList("157875196366160022"),
-				String.class);
 // 注意一下：接口内部如果异常也会触发降级
 //		throw new RuntimeException("发送异常了");
 	}
