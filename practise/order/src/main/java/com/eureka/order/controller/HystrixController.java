96b77f9fcf77b27b54a098aa4e4d82bac351de71
jason <shicong.zhang@juniorchina.com>
2019-01-23 14:52:11 +0800

    test

--- practise/order/src/main/java/com/eureka/order/controller/HystrixController.java
+++ practise/order/src/main/java/com/eureka/order/controller/HystrixController.java
@@ -23,11 +23,11 @@ public class HystrixController {
 //			@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1000")
 //	})
 
-	@HystrixCommand(commandProperties = {
-			@HystrixProperty(name = "circuitBreaker.enabled", value = "true"),  		                  		          //设置熔断    作用：及时切断故障接口，保证程序继续这行。 类似切断故障电路
-			@HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),	              //请求数达到后才计算
-			@HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000"),   //休眠时间窗
-			@HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60"),	         //错误率
+	@HystrixCommand(commandProperties = {                                                                                                 //熔断器的三个状态 ：closed → open → halfopen
+			@HystrixProperty(name = "circuitBreaker.enabled", value = "true"),  		                  		              //设置熔断    作用：及时切断故障接口，保证程序继续这行。 类似切断故障电路
+			@HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),	              //请求数达到后才计算，即：休眠时间窗内最小请求数
+			@HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000"),      //休眠时间窗
+			@HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60"),	             //错误率 即：在一个休眠时间窗内达到错误的百分比
 	},defaultFallback = "fallback")
 
 //	@HystrixCommand(defaultFallback = "fallback")
@@ -36,12 +36,12 @@ public class HystrixController {
 		if (number % 2 == 0) {
 			return "success";
 		}
-//        Thread.sleep(2000);
-//		return  "ok";
-		RestTemplate restTemplate = new RestTemplate();
-		return restTemplate.postForObject("http://127.0.0.1:8005/product/listForOrder",
-				Arrays.asList("157875196366160022"),
-				String.class);
+        Thread.sleep(2000);
+		return  "ok";
+//		RestTemplate restTemplate = new RestTemplate();
+//		return restTemplate.postForObject("http://127.0.0.1:8005/product/listForOrder",
+//				Arrays.asList("157875196366160022"),
+//				String.class);
 
 // 注意一下：接口内部如果异常也会触发降级
 //		throw new RuntimeException("发送异常了");
