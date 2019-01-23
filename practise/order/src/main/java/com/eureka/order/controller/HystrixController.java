0732b5859a9cdae2d74e4be5353a1095195d2e21
jason <shicong.zhang@juniorchina.com>
2019-01-23 14:29:22 +0800

    test

--- practise/order/src/main/java/com/eureka/order/controller/HystrixController.java
+++ practise/order/src/main/java/com/eureka/order/controller/HystrixController.java
@@ -2,6 +2,7 @@ package com.eureka.order.controller;
 
 import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,29 +18,30 @@ import java.util.Arrays;
 //@DefaultProperties(defaultFallback = "defaultFallback")
 public class HystrixController {
 
-	//超时配置  默认为1000ms = 1秒   如果配置文件配置了超时  →  时间那么此处就失效了
+	//超时配置  默认为1000ms = 1秒   如果配置文件配置了【超时 时间】那么此处就失效了    依赖隔离→线程池隔离  Hystrix自动实现了依赖隔离
 //	@HystrixCommand(commandProperties = {
 //			@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1000")
 //	})
 
-//	@HystrixCommand(commandProperties = {
-//			@HystrixProperty(name = "circuitBreaker.enabled", value = "true"),  				//设置熔断
-//			@HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),	//请求数达到后才计算
-//			@HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000"), //休眠时间窗
-//			@HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60"),	//错误率
-//	})
-	@HystrixCommand(defaultFallback = "fallback")
+	@HystrixCommand(commandProperties = {
+			@HystrixProperty(name = "circuitBreaker.enabled", value = "true"),  		                  		          //设置熔断    作用：及时切断故障接口，保证程序继续这行。 类似切断故障电路
+			@HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),	              //请求数达到后才计算
+			@HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000"),   //休眠时间窗
+			@HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60"),	         //错误率
+	},defaultFallback = "fallback")
+
+//	@HystrixCommand(defaultFallback = "fallback")
 	@GetMapping("/getProductInfoList")
 	public String getProductInfoList(@RequestParam("number") Integer number) throws InterruptedException {
 		if (number % 2 == 0) {
 			return "success";
 		}
-        Thread.sleep(2000);
-		return  "ok";
-//		RestTemplate restTemplate = new RestTemplate();
-//		return restTemplate.postForObject("http://127.0.0.1:8005/product/listForOrder",
-//				Arrays.asList("157875196366160022"),
-//				String.class);
+//        Thread.sleep(2000);
+//		return  "ok";
+		RestTemplate restTemplate = new RestTemplate();
+		return restTemplate.postForObject("http://127.0.0.1:8005/product/listForOrder",
+				Arrays.asList("157875196366160022"),
+				String.class);
 
 // 注意一下：接口内部如果异常也会触发降级
 //		throw new RuntimeException("发送异常了");
