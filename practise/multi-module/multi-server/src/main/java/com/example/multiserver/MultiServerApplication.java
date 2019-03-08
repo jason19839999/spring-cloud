de333f0005a1f75fd8ebf871b697bf3e9f38874e
zhongzunfa <973373278@qq.com>
2019-03-08 14:50:55 +0800

    test

--- practise/multi-module/multi-server/src/main/java/com/example/multiserver/MultiServerApplication.java
+++ practise/multi-module/multi-server/src/main/java/com/example/multiserver/MultiServerApplication.java
@@ -4,6 +4,7 @@ import com.example.multicommon.model.DecreaseStockInput;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
 
 @SpringBootApplication
@@ -24,6 +25,22 @@ public class MultiServerApplication {
 
 
     public static void main(String[] args) {
+
+        Map<String,Object> maps = new HashMap<String,Object>(100000000);
+        for(int i =0;i<10000000;i++){
+            maps.put(String.valueOf(i),  UUID.randomUUID().toString().replace("-", ""));
+        }
+
+        Set<String> sets = new HashSet<String>(100000000);
+
+        Set<Map.Entry<String, Object>> set1 = maps.entrySet();
+        Iterator<Map.Entry<String, Object>> iterator1 = set1.iterator();
+        while (iterator1.hasNext()) {
+            Map.Entry<String, Object> entry = iterator1.next();
+            sets.add(String.valueOf(entry.getValue()));
+        }
+
+        int count = sets.size();
         DecreaseStockInput decreaseStockInput = new DecreaseStockInput();
         decreaseStockInput.setProductQuantity(10000);
         decreaseStockInput.setProductId("L24LJ64LHOGMVNOI213");
